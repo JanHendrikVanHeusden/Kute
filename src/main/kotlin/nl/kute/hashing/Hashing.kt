f7c110d2641f0904adcaf440c020f339ee4ed3eb
Jan-Hendrik van Heusden <jan-hendrik@bit-academy.nl>
2022-02-08 09:57:14 +0100

    Hashing accepts different encodings

--- src/main/kotlin/nl/kute/hashing/Hashing.kt
+++ src/main/kotlin/nl/kute/hashing/Hashing.kt
@@ -3,30 +3,79 @@ package nl.kute.hashing
 import nl.kute.hashing.ThreadDigestHelper.Companion.threadCrc32c
 import nl.kute.hashing.ThreadDigestHelper.Companion.threadDigestByAlgorithm
 import java.math.BigInteger
+import java.nio.charset.Charset
 import java.security.MessageDigest
 import java.util.zip.CRC32C as javaUtilCRC32C
 
+/**
+ * Create a hex String based on the [input] object's [hashCode] method
+ * @param input The [Any] object to create as hash String for
+ * @return The [input]'s [hashCode] as a hexadecimal String
+ *         consisting of characters `0..9`, `a..f`, optionally prepended by a minus sign `-`
+ */
 fun javaHashString(input: Any): String = input.hashCode().toString(16)
 
-fun hashString(input: String, digestMethod: DigestMethod): String {
+/**
+ * Create a hash String for the given [input] String
+ * @param input The [String] to create as hash for
+ * @param digestMethod The [DigestMethod] to create the hash with
+ * @param charset The [Charset] of the input [String]; default is [Charset.defaultCharset].
+ * @return The hash String, as a hexadecimal String
+ *         consisting of characters `0..9`, `a..f`, optionally prepended by a minus sign `-`
+ */
+fun hashString(input: String, digestMethod: DigestMethod, charset: Charset = Charset.defaultCharset()): String {
     return when (digestMethod) {
         DigestMethod.JAVA_HASHCODE ->
             javaHashString(input)
 
         DigestMethod.CRC32C -> {
             with(threadCrc32c.get()) {
-                this.update(input.toByteArray())
+                this.update(input.toByteArray(charset))
                 this.value.toString(16)
             }
         }
         else -> {
             require(!digestMethod.algorithm.isNullOrBlank()) { "No Digest algorithm specified" }
             with(threadDigestByAlgorithm[digestMethod.algorithm]!!.get()) {
-                this.update(input.toByteArray())
+                this.update(input.toByteArray(charset))
                 BigInteger(this.digest()).toString(16)
             }
         }
     }
+}
+
+/**
+ * Hash algorithms; typical usage is to avoid exposing sensitive or personally identifiable data in logging etc.
+ * * **Note that hashing data like this is *NOT* meant to be a security mechanism.**
+ *     It is just a way to avoid that plain text data is exposed in logging etc.,
+ *     and should only be used like that.
+ *     Securing data would require much more hardening than this library is intended for.
+ * * Given that, the hashing algorithms of this enum are selected for best performance, not for security.
+ */
+enum class DigestMethod(val algorithm: String? = null) {
+    /**
+     * Simply using the java hashCode.
+     * Length is 8 or 9 when represented as a hex String (9 in case of negative values).
+     */
+    JAVA_HASHCODE(null),
+
+    /**
+     * CRC32C is a fast hash methods with compact output (length 1 to 8), but security is nearly absent.
+     * The resulting hex String may be 0, but never a negative value.
+     */
+    CRC32C(null),
+
+    /**
+     * SHA1 is considered as one of the better performing hash methods; but not considered secure.
+     * Length is 39 or 40 when represented as a hex String, optionally prepended by a minus sign.
+     */
+    SHA1("SHA-1"),
+
+    /**
+     * MD5 is considered as one of the better performing hash methods; but not considered secure
+     * Length is 39 or 40 when represented as a hex String, optionally prepended by a minus sign.
+     */
+    MD5("MD5")
 }
 
 private class ThreadDigestHelper {
@@ -50,26 +99,3 @@ private class ThreadDigestHelper {
     }
 
 }
-
-/**
- * Hash algorithms to avoid exposing sensitive or personally identifiable data in logging etc.
- * * **Note that hashing data like this is *NOT* meant to be a security mechanism.**
- *     It is just a way to avoid that plain text data is exposed in logging etc.,
- *     and should only be used like that.
- *     Securing data would require much more hardening than this library can bring or even is intended for.
- * * Given that, the hashing algorithms of this enum are selected for best performance, not for security.
- */
-enum class DigestMethod(val algorithm: String? = null) {
-    /** Simply using the java hashCode. Length is 8 or 9 (9 in case of negative values) */
-    JAVA_HASHCODE(null),
-
-    /** CRC32C is a fast hash methods with compact output (length 8), but security is nearly absent */
-    CRC32C(null),
-
-    /** SHA1 is considered as one of the better performing hash methods; but not considered secure */
-    SHA1("SHA-1"),
-
-    /** MD5 is considered as one of the better performing hash methods; but not considered secure */
-    MD5("MD5")
-}
-
--- src/test/kotlin/nl/kute/hashing/HashingTest.kt
+++ src/test/kotlin/nl/kute/hashing/HashingTest.kt
@@ -2,6 +2,7 @@ package nl.kute.hashing
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.nio.charset.Charset
 import java.time.LocalDateTime
 import kotlin.random.Random
 
@@ -9,10 +10,11 @@ internal class HashingTest {
 
     private val testStringShort = "my test string"
 
-    // String of length 335k
+    // String of length ~335k
     private val testStringLong = mutableListOf("abcdefghijklmnopqrstuvwxyz1234567890-=`~!@#$%^&*()_+{}[];:',./<>?")
         .also { list -> repeat(5000) { list.add(list[0]) } }.joinToString()
 
+    /** Expected output patterns by digest method */
     private val digestMethodPatterns =
         mapOf(
             DigestMethod.JAVA_HASHCODE to Regex("^-?(\\d|[a-f]){1,8}$"),
@@ -20,6 +22,48 @@ internal class HashingTest {
             DigestMethod.SHA1 to Regex("^-?(\\d|[a-f]){39,40}$"),
             DigestMethod.MD5 to Regex("^-?(\\d|[a-f]){31,32}$")
         )
+
+    @Test
+    fun `test that results of hash methods adhere to expected formats`() {
+        var hashResults: MutableSet<String>
+
+        digestMethodPatterns.forEach { digestMethod, pattern ->
+            hashResults = mutableSetOf()
+
+            // nested helper method for assertion
+            fun assertHashFormat(input: String) {
+                // assert that results adhere to expected format
+                with(hashString(input, digestMethod)) {
+                    assertThat(pattern.matches(this))
+                        .`as`("$digestMethod hash result [$this] does not match \"${pattern.pattern}\"; input = \"$input\"")
+                        .isTrue
+                    // convert from hex string to numeric; wrong format would cause NumberFormatException
+                    this.toBigInteger(radix = 16)
+
+                    // duplicates (collisions) are definitely possible and acceptable,
+                    // especially for the smaller hashes (java hash and CRC32(c)).
+                    // But with the limited numbers of this test, we know that there should be no collisions
+                    assertThat(hashResults.add(input))
+                        .`as`("$digestMethod should not cause duplicate result!")
+                        .isTrue
+                }
+            }
+
+            var strMinimal = ""
+            repeat(10) {
+                assertHashFormat(strMinimal)
+                strMinimal += " "
+            }
+
+            assertHashFormat(testStringShort)
+
+            var strLong = testStringLong
+            repeat(40) {
+                strLong += "something"
+                assertHashFormat(strLong)
+            }
+        }
+    }
 
     @Test
     fun `test that String hashing with JAVA_HASHCODE digest yields same result as java hashCode`() {
@@ -40,55 +84,30 @@ internal class HashingTest {
     }
 
     @Test
-    fun `test that results of hash methods adhere to expected format for digester`() {
-        var hashResults: MutableSet<String>
-        digestMethodPatterns.forEach { digestMethod, pattern ->
-            hashResults = mutableSetOf()
-            // nested helper method for assertion
-            fun assertHashFormat(input: String) {
-                // assert that results adhere to expected format
-                with(hashString(input, digestMethod)) {
-                    assertThat(pattern.matches(this))
-                        .`as`("$digestMethod hash result [$this] does not match \"${pattern.pattern}\"; input = \"$input\"")
-                        .isTrue
-                }
-                // duplicates (collisions) are definitely possible and acceptable, especially for java hash and CRC32(c)
-                // but with the limited numbers of this test, we know that there should be no collisions
-                assertThat(hashResults.add(input))
-                    .`as`("$digestMethod gives duplicate result!")
-                    .isTrue
-            }
-
-            var strMinimal = ""
-            repeat(10) {
-                assertHashFormat(strMinimal)
-                strMinimal += " "
-            }
-
-            assertHashFormat(testStringShort)
-
-            var strLong = testStringLong
-            repeat(40) {
-                strLong += "something"
-                assertHashFormat(strLong)
-            }
-        }
-    }
-
-    @Test
-    fun `verify that all algorithms yield a hex String`() {
-        DigestMethod.values().forEach {
-            with(hashString("some string", it)) {
-                // correct format succeeds; wrong format would cause NumberFormatException
-                this.toBigInteger(radix = 16)
-            }
-        }
-    }
-
-    @Test
     fun `test that Object hashing with javaHashCode yields same result as java hashCode`() {
         listOf(Any(), LocalDateTime.now(), Random.nextInt(), Random.nextBytes(200)).forEach {
             assertThat(javaHashString(it)).isEqualTo(it.hashCode().toString(16))
+        }
+    }
+
+    @Test
+    fun `test that encodings are taken into account as expected`() {
+        val str1 = "¥⒂"
+        val str2 = "¬"
+        // java hash is wilfully ignorant of encoding
+        var hashGB2312 = hashString(str1, DigestMethod.JAVA_HASHCODE, charset = Charset.forName("GB2312"))
+        var hashUTF8 = hashString(str1, DigestMethod.JAVA_HASHCODE, charset = Charset.forName("UTF8"))
+        assertThat(hashGB2312).isEqualTo(hashUTF8)
+
+        // other digesters take character set in account
+        DigestMethod.values().filter { it != DigestMethod.JAVA_HASHCODE }.forEach {
+            hashGB2312 = hashString(str1, it, charset = Charset.forName("GB2312"))
+            hashUTF8 = hashString(str1, it, charset = Charset.forName("UTF8"))
+            assertThat(hashGB2312).isNotEqualTo(hashUTF8)
+
+            val hashISO88591 = hashString(str2, DigestMethod.MD5, Charset.forName("ISO-8859-1"))
+            hashUTF8 = hashString(str2, DigestMethod.MD5, Charset.forName("UTF8"))
+            assertThat(hashISO88591).isNotEqualTo(hashUTF8)
         }
     }
 
