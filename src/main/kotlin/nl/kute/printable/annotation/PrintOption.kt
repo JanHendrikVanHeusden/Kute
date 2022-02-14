76d57524e4555922f51068f9d15437e2062a19d2
Jan-Hendrik van Heusden <jan-hendrik@bit-academy.nl>
2022-02-14 09:52:58 +0100

    some refactoring

---
+++ src/main/kotlin/nl/kute/printable/annotation/PrintOption.kt
@@ -0,0 +1,20 @@
+package nl.kute.printable.annotation
+
+import nl.kute.printable.Printable
+
+const val printableDefaultNullString: String = "null"
+const val printableDefaultMaxLength: Int = 500
+
+/**
+ * The [PrintOption] annotation can be placed on classes that implement [Printable],
+ * or on properties of these classes.
+ * It allows specifying how property values are to be parsed in the [Printable.asString] return value.
+ */
+@Target(AnnotationTarget.CLASS, AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION)
+@MustBeDocumented
+annotation class PrintOption(
+    /** How to show nulls? Default is "`"null"`" (by [printableDefaultNullString]), but you may opt for something else */
+    val showNullAs: String = printableDefaultNullString,
+    /** The maximum length of the output. Default is 500 (by [printableDefaultMaxLength]). 0 means: an empty String; negative values mean: no maximum. */
+    val maxLength: Int = printableDefaultMaxLength
+)
--- src/main/kotlin/nl/kute/reflection/PropertyResolver.kt
+++ src/main/kotlin/nl/kute/reflection/PropertyResolver.kt
@@ -1,49 +1,49 @@
 package nl.kute.reflection
 
+import kotlin.reflect.KProperty0
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.isAccessible
 
-class PropertyResolver {
+/**
+ * @param prop the property to get the value of
+ * @return the property's value as of the `this` receiver
+ */
+fun Any.getPropValue(prop: KProperty1<Any, *>): Any? = prop.get(this)
 
-    /**
-     * @param prop the property to get the value of
-     * @return the property's value as of the `this` receiver
-     */
-    fun Any.getPropValue(prop: KProperty1<Any, *>): Any? = prop.get(this)
+fun getPropValue(prop: KProperty0<*>): Any? = prop.get()
 
-    /**
-     * Get the properties from the hierarchy (see [classHierarchy]
-     */
-    fun Any.propertiesFromHierarchy(
-        includeNonPublic: Boolean = false,
-        propertyFilter: (KProperty1<out Any, *>) -> Boolean = { true }
-    ): Collection<KProperty1<out Any, *>> {
-        val classHierarchy = this.classHierarchy()
-        val linkedHashSet: LinkedHashSet<KProperty1<out Any, *>> = linkedSetOf()
-        return linkedHashSet.also { theSet ->
-            theSet.addAll(
-                classHierarchy
-                    .map { kClass -> kClass.memberProperties }.flatten()
-                    // Property overrides would cause duplicate names. We want to keep the subclass property only.
-                    // But distinctBy keeps the first value (would be the topmost superclass property).
-                    // To keep the last ones (deepest subclass), reverse it, then call distinctBy
-                    .reversed().distinctBy { prop -> prop.name }
-                    .reversed() // reverse again to normal order
-                    .onEach { prop ->
-                        if (includeNonPublic) {
-                            try {
-                                prop.isAccessible = true // to access private or protected ones
-                            } catch (e: Exception) {
-                                // probably due to SecurityManager and/or jig saw boundaries;
-                                // we must adhere, so ignore
-                            }
+/**
+ * Get the properties from the hierarchy (see [classHierarchy]
+ */
+fun Any.propertiesFromHierarchy(
+    includeNonPublic: Boolean = false,
+    propertyFilter: (KProperty1<out Any, *>) -> Boolean = { true }
+): List<KProperty1<out Any, *>> {
+    val classHierarchy = this.classHierarchy()
+    val linkedHashSet: LinkedHashSet<KProperty1<out Any, *>> = linkedSetOf()
+    return linkedHashSet.also { theSet ->
+        theSet.addAll(
+            classHierarchy
+                .map { kClass -> kClass.memberProperties }.flatten()
+                // Property overrides would cause duplicate names. We want to keep the subclass property only.
+                // But distinctBy keeps the first value (would be the topmost superclass property).
+                // To keep the last ones (deepest subclass), reverse it, then call distinctBy
+                .reversed().distinctBy { prop -> prop.name }
+                .reversed() // reverse again to normal order
+                .onEach { prop ->
+                    if (includeNonPublic) {
+                        try {
+                            prop.isAccessible = true // to access private or protected ones
+                        } catch (e: Exception) {
+                            // probably due to SecurityManager and/or jig saw boundaries;
+                            // we must adhere, so ignore
                         }
                     }
-                    .filter { it.isAccessible }
-                    .filter { propertyFilter(it) }
-            )
-        }
-    }
-
+                }
+                .filter { it.isAccessible }
+                .filter { propertyFilter(it) }
+        )
+    }.toList()
 }
+
