c823d1a69001969025d0f0932fc9cb0616774971
Jan-Hendrik van Heusden <jan-hendrik@bit-academy.nl>
2022-02-14 09:54:13 +0100

    Draft implementation of annotation resolving Initial (not working) test of PropertyResolver

---
+++ src/test/kotlin/nl/kute/reflection/PropertyResolverTest.kt
@@ -0,0 +1,17 @@
+package nl.kute.reflection
+
+import org.junit.jupiter.api.Test
+
+internal class PropertyResolverTest {
+
+    @Test
+    fun propertiesFromHierarchy() {
+        TODO("Not implemented yet")
+    }
+
+    @Test
+    fun getPropValue() {
+        TODO("Not implemented yet")
+    }
+
+}
