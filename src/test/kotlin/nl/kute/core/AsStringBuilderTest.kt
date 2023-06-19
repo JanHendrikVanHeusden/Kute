c1f8c1c67de28dca53c0a42d404da44896fe20b9
Jan-Hendrik van Heusden <jan-hendrik.van.heusden@ordina.nl>
2023-06-19 17:46:35 +0200

    Added initial skeleton for AsStringBuilderTest

---
+++ src/test/kotlin/nl/kute/core/AsStringBuilderTest.kt
@@ -0,0 +1,38 @@
+package nl.kute.core
+
+import org.junit.jupiter.api.Test
+
+internal class AsStringBuilderTest {
+
+    @Test
+    fun getObjectReference() {
+    }
+
+    @Test
+    fun withAlsoProperties() {
+    }
+
+    @Test
+    fun withAlsoNamed() {
+    }
+
+    @Test
+    fun onlyProperties() {
+    }
+
+    @Test
+    fun withOnlyNames() {
+    }
+
+    @Test
+    fun exceptProperties() {
+    }
+
+    @Test
+    fun exceptPropertyNames() {
+    }
+
+    @Test
+    fun asStringBuilder() {
+    }
+}
