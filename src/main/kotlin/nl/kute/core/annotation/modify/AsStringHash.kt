f13a242a3dfdcc61ace40f93881cf54079eac6bc
Jan-Hendrik van Heusden <janhvh@gmail.com>
2023-07-01 10:26:23 +0200

    Surround hash value by # characters. WIP: several tests failing

--- src/main/kotlin/nl/kute/core/annotation/modify/AsStringHash.kt
+++ src/main/kotlin/nl/kute/core/annotation/modify/AsStringHash.kt
@@ -7,7 +7,8 @@ import kotlin.annotation.AnnotationRetention.RUNTIME
 
 /**
  * The [AsStringHash] annotation can be placed on properties to indicate that the property is included
- * in the return value of [nl.kute.core.asString], but with its String value replaced by the String's hash value.
+ * in the return value of [nl.kute.core.asString], but with its String value replaced by the String's hash value,
+ * the hash value surrounded by `#` characters.
  * * Usage of a hash method allows tracking of values across multiple entries, without exposing the original value.
  * * Typical usage is to keep sensitive or personally identifiable data out of logging etc.
  *     * This may limit exposure of such data, but on its own it must not be considered a security feature.
@@ -27,4 +28,4 @@ import kotlin.annotation.AnnotationRetention.RUNTIME
 annotation class AsStringHash(val digestMethod: DigestMethod = DigestMethod.CRC32C)
 
 internal fun AsStringHash?.hashString(strVal: String?): String? =
-    if (this == null) strVal else hashString(strVal, digestMethod)
+    if (this == null) strVal else "#${hashString(strVal, digestMethod)}#"
