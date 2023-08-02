8b8661e69e17eaef79130970d76d5b0bde528c3a
Jan-Hendrik van Heusden <janhvh@gmail.com>
2023-08-02 19:22:58 +0200

    Added test for propsWithAnnotationsCacheByClass
    To do:
    * test for classToStringMethodCache

---
+++ src/test/kotlin/nl/kute/core/property/PropertyValueResolverTest.kt
@@ -0,0 +1,121 @@
+package nl.kute.core.property
+
+import nl.kute.core.annotation.modify.AsStringHash
+import nl.kute.core.annotation.option.AsStringOption
+import nl.kute.core.asString
+import nl.kute.hashing.DigestMethod
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PropertyValueResolverTest {
+
+    @Test
+    fun `property and annotation resolution should be cached`() {
+        // arrange
+        propsWithAnnotationsCacheByClass.reset()
+        assertThat(propsWithAnnotationsCacheByClass.size).isZero
+
+        open class TestClass {
+            @AsStringHash(DigestMethod.SHA1)
+            open val hashed = "I will be hashed"
+            @AsStringOption(propMaxStringValueLength = 9)
+            val withOption = "I will be truncated"
+            override fun toString() = asString()
+        }
+
+        @AsStringOption(showNullAs = "<null>")
+        open class SubClass: TestClass() {
+            @AsStringHash(DigestMethod.CRC32C)
+            override val hashed: String = super.hashed
+        }
+
+        @AsStringOption(propMaxStringValueLength = 5000)
+        class SubSubClass: SubClass() {
+            @Suppress("unused")
+            val aSubSubProperty  = 12
+        }
+
+        // act - TestClass
+        repeat(3) { TestClass().asString() }
+        // assert
+        assertThat(propsWithAnnotationsCacheByClass.cache)
+            .hasSize(1)
+            .containsKey(TestClass::class)
+        var propAnnotationMap = propsWithAnnotationsCacheByClass[TestClass::class]!!
+        assertThat(propAnnotationMap)
+            .hasSize(2)
+        assertThat(propAnnotationMap.keys.map { it.name })
+            .containsExactlyInAnyOrder("hashed", "withOption")
+
+        var annotations = propAnnotationMap.filterKeys { it.name == "hashed" }.values.first()
+        assertThat(annotations)
+            .containsExactlyInAnyOrder(
+                AsStringOption.defaultOption,
+                AsStringHash(DigestMethod.SHA1)
+            )
+        annotations = propAnnotationMap.filterKeys { it.name == "withOption" }.values.first()
+        assertThat(annotations)
+            .containsExactly(
+                AsStringOption(propMaxStringValueLength = 9)
+            )
+
+        // act - SubClass
+        repeat(3) {SubClass().asString()}
+        // assert
+        assertThat(propsWithAnnotationsCacheByClass.cache)
+            .hasSize(2)
+            .containsKey(SubClass::class)
+        propAnnotationMap = propsWithAnnotationsCacheByClass[SubClass::class]!!
+        assertThat(propAnnotationMap)
+            .hasSize(2)
+        assertThat(propAnnotationMap.keys.map { it.name })
+            .containsExactlyInAnyOrder("hashed", "withOption")
+
+        annotations = propAnnotationMap.filterKeys { it.name == "hashed" }.values.first()
+        assertThat(annotations)
+            .containsExactlyInAnyOrder(
+                AsStringOption(showNullAs = "<null>"),
+                // ignores "override" on subclass
+                AsStringHash(DigestMethod.SHA1)
+            )
+
+        annotations = propAnnotationMap.filterKeys { it.name == "withOption" }.values.first()
+        assertThat(annotations)
+            .containsExactly(
+                AsStringOption(propMaxStringValueLength = 9)
+            )
+
+        // act - SubSubClass
+        repeat(3) {SubSubClass().asString()}
+        // assert
+        assertThat(propsWithAnnotationsCacheByClass.cache)
+            .hasSize(3)
+            .containsKey(SubSubClass::class)
+        propAnnotationMap = propsWithAnnotationsCacheByClass[SubSubClass::class]!!
+        assertThat(propAnnotationMap)
+            .hasSize(3)
+        assertThat(propAnnotationMap.keys.map { it.name })
+            .containsExactlyInAnyOrder("hashed", "withOption", "aSubSubProperty")
+
+        annotations = propAnnotationMap.filterKeys { it.name == "hashed" }.values.first()
+        assertThat(annotations)
+            .containsExactlyInAnyOrder(
+                AsStringOption(propMaxStringValueLength = 5000),
+                // ignores "override" on subclass
+                AsStringHash(DigestMethod.SHA1)
+            )
+
+        annotations = propAnnotationMap.filterKeys { it.name == "withOption" }.values.first()
+        assertThat(annotations)
+            .containsExactly(
+                AsStringOption(propMaxStringValueLength = 9)
+            )
+
+        annotations = propAnnotationMap.filterKeys { it.name == "aSubSubProperty" }.values.first()
+        assertThat(annotations)
+            .containsExactlyInAnyOrder(
+                AsStringOption(propMaxStringValueLength = 5000)
+            )
+    }
+
+}
